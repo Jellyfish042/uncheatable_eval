43adfe1528e42efe9fd7967b830c73c5bde4b476
Jellyfish <1604466216@qq.com>
2024-05-24 20:43:35 +0800

    python3

--- utils/download_models.py
+++ utils/download_models.py
@@ -2,9 +2,9 @@ import subprocess
 import os
 
 model_names = [
-    'BlinkDL/rwkv-6-world/RWKV-x060-World-1B6-v2.1-20240328-ctx4096.pth',
+    # 'BlinkDL/rwkv-6-world/RWKV-x060-World-1B6-v2.1-20240328-ctx4096.pth',
     # 'BlinkDL/rwkv-5-world/RWKV-5-World-1B5-v2-20231025-ctx4096.pth',
-    # 'stabilityai/stablelm-2-1_6b',
+    'stabilityai/stablelm-2-1_6b',
     # 'h2oai/h2o-danube-1.8b-base',
     # 'TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T',
     # 'EleutherAI/pythia-1.4b-v0',
@@ -54,7 +54,7 @@ cache_dir = '../models/temp/'
 
 for name in model_names:
     args = [
-        'python', 'download_single_model.py',
+        'python3', 'download_single_model.py',
         '--model_name', name,
         '--cache_dir', cache_dir,
     ]
