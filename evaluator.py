9b4cc74b0f0e0f38a5b9a3f1b05aac17c228201e
Jellyfish042 <1604466216@qq.com>
2025-12-26 13:52:06 +0800

    cleanup

--- evaluator.py
+++ evaluator.py
@@ -387,13 +387,13 @@ class Evaluator:
 
             byte_index = 0
             if track_byte_wise_data:  # track byte-wise data is not compatible with chunking, so here we will get the whole sequence's loss
-                # token_strings = tokenizer.convert_ids_to_tokens(input_chunk.squeeze(0).tolist()[1:])
                 per_token_bytes = get_decoded(tokenizer, sample)
                 all_bytes = [byte for token in per_token_bytes for byte in token]
-                assert all_bytes == list(sample.encode("utf-8")), "All bytes are not the same"
+                assert all_bytes == list(
+                    sample.encode("utf-8")
+                ), "All bytes are not the same, this model and tokenizer are not compatible with tracking byte-wise data mode"
                 assert len(per_token_bytes) == loss.shape[0], "Number of tokens and loss are not the same"
                 for l, byte_values in zip(loss, per_token_bytes):
-                    # byte_values = [tokenizer.byte_decoder[c] for c in token_str]
                     per_byte_loss = l.item() / len(byte_values)
                     for _ in range(len(byte_values)):
                         byte_wise_loss_sum[byte_index] += per_byte_loss
